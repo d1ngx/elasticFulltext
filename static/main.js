805a37794aa4e35f7c6dcc4fea3be24b2694a63d
d1ng <33094678+d1ngx@users.noreply.github.com>
2026-09-14 00:29:20 +0800

    Add static/main.js

---
+++ static/main.js
@@ -0,0 +1,27 @@
+kodReady.push(function(){
+	if (window.__elasticFulltextSearchUI) return;
+	window.__elasticFulltextSearchUI = true;
+	var css = '.file-list-list .file.file-search-match.has-file-cover .search-match-content .file-cover:not(:has(.picture)){display:none!important}'
+		+ '.file-list-list .file.file-search-match.has-file-cover .search-match-content:not(:has(.picture)) .match-text{display:block!important;height:auto!important;margin:0 5px 5px 25px!important}';
+	if (!document.getElementById('elastic-fulltext-search-style')) {
+		var style = document.createElement('style');
+		style.id = 'elastic-fulltext-search-style';
+		style.type = 'text/css';
+		style.appendChild(document.createTextNode(css));
+		document.head.appendChild(style);
+	}
+
+	function hideTypeIconCover($root){
+		($root && $root.find ? $root : $(document)).find('.file.file-search-match.has-file-cover').each(function(){
+			var $file = $(this);
+			var $cover = $file.find('.search-match-content .file-cover');
+			if (!$cover.length || $cover.find('.picture').length) return;
+			$cover.remove();
+			$file.removeClass('has-file-cover');
+		});
+	}
+
+	Events.bind('explorer.path.list.after', function(){
+		hideTypeIconCover($('.file-continer'));
+	});
+});
